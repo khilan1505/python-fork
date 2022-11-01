6d72d7b24aaebf83d804ec11d65544a4f1a9a1da
Ruturajsinh Bihola <ruturajbihola121@gmail.com>
2022-11-01 21:47:11 +0530

    append text for learning append

---
+++ break.py
@@ -0,0 +1,11 @@
+#break statement
+#it help for come out of loop when neccesary
+
+for a in range(2,10):
+    print(a)
+    if a==5:
+        break
+for a in range(2,10):
+    print(a)
+else:
+    print("hiii")
